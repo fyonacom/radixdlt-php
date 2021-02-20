45f4a6c4c5b08e6703d273f3501d0efccf695bc2
fyonacom <ben@fyona.com>
2021-02-20 18:26:48 +0100

    My first DSON serialization!

--- src/Types/Particles/SystemParticle.php
+++ src/Types/Particles/SystemParticle.php
@@ -13,7 +13,15 @@ declare(strict_types=1);
 
 namespace Techworker\RadixDLT\Types\Particles;
 
-class SystemParticle extends AbstractParticle
+use BN\BN;
+use CBOR\AbstractCBORObject;
+use CBOR\InfiniteMapObject;
+use CBOR\TextStringObject;
+use CBOR\UnsignedIntegerObject;
+use Techworker\RadixDLT\Serialization\Interfaces\FromJsonInterface;
+use Techworker\RadixDLT\Serialization\Interfaces\ToDsonInterface;
+
+class SystemParticle extends AbstractParticle implements FromJsonInterface, ToDsonInterface
 {
     protected function __construct(
         protected int $epoch,
@@ -22,29 +30,32 @@ class SystemParticle extends AbstractParticle
     ) {
     }
 
-    public function getAddresses(): array
-    {
-        return [];
-    }
-
-    public static function fromJson(array | string $json): self
+    public static function fromJson(array|string $json): static
     {
         if (is_string($json)) {
             throw new \InvalidArgumentException('Invalid.');
         }
 
-        $epoch = (int) $json['epoch'];
-        $view = (int) $json['view'];
-        $timestamp = (int) $json['timestamp'];
-        return new self($epoch, $view, $timestamp);
+        $data = [
+            'epoch' => (int)$json['epoch'],
+            'view' => (int)$json['view'],
+            'timestamp' => (int)$json['timestamp']
+        ];
+        return new SystemParticle(...$data);
     }
 
-    public function toJson(): array | string
+    public function toDson(): AbstractCBORObject
     {
-        $json = [];
-        $json['epoch'] = $this->epoch;
-        $json['view'] = $this->view;
-        $json['timestamp'] = $this->timestamp;
-        return $json;
+        $mo = new InfiniteMapObject();
+        $mo->append(new TextStringObject('epoch'), UnsignedIntegerObject::create($this->epoch));
+        $mo->append(new TextStringObject('serializer'), new TextStringObject('radix.particles.system_particle'));
+
+        // its a millisecond timestamp thats > 32bit, so we need to apply a small trick
+        // to get it to work, otherwise the cbor library will throw an error
+        $ts = hexToString((string)(new BN((string)$this->timestamp))->toString(16, 8));
+        $mo->append(new TextStringObject('timestamp'),
+                    UnsignedIntegerObject::createObjectForValue(27, $ts));
+        $mo->append(new TextStringObject('view'), UnsignedIntegerObject::create(1000));
+        return $mo;
     }
 }
